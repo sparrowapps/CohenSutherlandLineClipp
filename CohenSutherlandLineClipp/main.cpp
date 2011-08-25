bd9c0c4ea076ac1b34a8cd2b6e95b432c2fa569d
최준호 <sparrow@sparrow.local>
2011-08-25 17:09:28 +0900

    Initial Commit

---
+++ CohenSutherlandLineClipp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CohenSutherlandLineClipp
+//
+//  Created by 최준호 on 11. 8. 25..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
